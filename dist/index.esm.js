a2c0b1adc3cc161df3d16abc8c74bb91a696f834
Ali <adgk.dev@gmail.com>
2021-03-05 14:05:53 +0000

    Initial commit

---
+++ dist/index.esm.js
@@ -0,0 +1,2 @@
+export{n as default}from"./index-9abee4a4.js";import"@tensorflow/tfjs-core";import"@tensorflow/tfjs-backend-webgl";import"@tensorflow-models/face-landmarks-detection";import"@tensorflow-models/handpose";
+//# sourceMappingURL=index.esm.js.map
