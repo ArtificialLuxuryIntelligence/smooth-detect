87b9d39c2b93697450000d3d32f496427c8906e6
Ali <adgk.dev@gmail.com>
2021-03-12 17:17:36 +0000

    removed peer dependencies

--- dist/index.esm.js
+++
@@ -1,2 +0,0 @@
-export{n as default}from"./index-9abee4a4.js";import"@tensorflow/tfjs-core";import"@tensorflow/tfjs-backend-webgl";import"@tensorflow-models/face-landmarks-detection";import"@tensorflow-models/handpose";
-//# sourceMappingURL=index.esm.js.map
